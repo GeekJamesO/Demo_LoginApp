6bfcd788bbf19d6db3246c5c6968ebeea4d28c24
JamesORourke <geekjamesorourkejr@icloud.com>
2017-08-16 12:09:52 -0700

    Pre regex validation

--- apps/login_app/models.py
+++ apps/login_app/models.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+import re
 
 class UserManager(models.Manager):
     def RegVal(self, PostData):
-        pass
-
-    pass
+        results = {"status": True, 'errors': [] }
+        if len(PostData['first_name']) < 2:
+            results['errors'].append("first name must be 3 or more characters")
+        if len(PostData['last_name']) < 2:
+            results['errors'].append("last name must be 3 or more characters")
+        if re.(PostData['email']) :
+            results['errors'].append("first name must be 3 or more characters")
+        return results;
 
 class User(models.Model):
     first_name = models.CharField(max_length=100)
--- apps/login_app/views.py
+++ apps/login_app/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from models import User
 
 # Create your views here.
 def index(request):
@@ -8,8 +9,5 @@ def index(request):
     return render(request, "login_app/index.html", context)
 
 def register(request):
-    print "in register"
-    print request.POST
-    print "First name:" , request.POST['first_name']
-
+    print User.objects.RegVal(request.POST)
     return render(request, "login_app/index.html")
